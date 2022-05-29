b3c88ce74c8fa95e11405eee3ff5400e05f4ecfd
WillyAgustinoEfendi <willyagustinoefendi12@gmail.com>
2022-05-30 04:38:10 +0700

    API GET - List Image

--- routes/media.js
+++ routes/media.js
@@ -5,11 +5,30 @@ const base64Img = require("base64-img");
 
 const { Media } = require("../models");
 
+router.get("/", async (req, res) => {
+  const media = await Media.findAll({
+    attributes: ["id", "image"],
+  });
+
+  const mappedMedia = media.map((m) => {
+    m.image = `${req.get("host")}/${m.image}`;
+    return m;
+  });
+
+  return res.json({
+    status: "success",
+    data: mappedMedia,
+  });
+});
+
 router.post("/", (req, res) => {
   const image = req.body.image;
 
   if (!isBase64(image, { mimeRequired: true })) {
-    return res.status(400).json({ status: "error", message: "invalid base64" });
+    return res.status(400).json({
+      status: "Invalid image",
+      message: "The image is not a valid base64 string",
+    });
   }
 
   base64Img.img(image, "./public/images", Date.now(), async (err, filepath) => {
@@ -25,7 +44,7 @@ router.post("/", (req, res) => {
       status: "success",
       data: {
         id: media.id,
-        image: `${"host"}/images/${filename}`,
+        image: `${req.get("host")}/images/${filename}`,
       },
     });
   });
